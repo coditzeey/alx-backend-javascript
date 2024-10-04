f0a67ce9ad6b73d43f959616a29763b35709e7b0
coditzeey <www.georgeokafor99@gmail.com>
2024-10-04 15:18:25 -0400

    ECMASCRIPT

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+// ilterate through
+export default function iterateThroughObject(reportWithIterator) {
+    const employees = [];
+    for (const item of reportWithIterator) {
+      employees.push(item);
+    }
+    return employees.join(' | ');
+  }
+  
